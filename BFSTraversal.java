cb03526684356e0d7000019e7d9ec5b418a756d4
LloydDrums <lkbamoah@gmail.com>
2025-12-09 16:17:55 -0500

    Hello

--- BFSTraversal.java
+++ BFSTraversal.java
@@ -1,7 +1,8 @@
 public class BFSTraversal {
 
-    //find people you may know using BFS
-    // Determin the minimum bumber of connectrions between  two users.
+    // find people you may know using BFS
+    // Determine the minimum number of connections between two users.
+    // based off of NetworkGraph.java :)
 
     
 }
