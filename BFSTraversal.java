181d06c32b3be14236583c69a4684a6a3a5b6c8c
Malik <mh50914p@pace.edu>
2025-12-09 17:04:50 -0500

    ok

--- BFSTraversal.java
+++
@@ -1,8 +0,0 @@
-public class BFSTraversal {
-
-    // find people you may know using BFS
-    // Determine the minimum number of connections between two users.
-    // based off of NetworkGraph.java :)
-
-    
-}
